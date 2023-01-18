f654ec6373ffa6b6f4e7e0cd468d429124510335
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2023-01-18 16:34:32 +0100

    image chemin

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,15 +5,38 @@ const identifiant = urlParams.get('id')// elle renvoie une chaine de caractere p
 const photographers = await getPhotographers();
 let getPhotographer = () => {
     let photographer = photographers.photographers.find(photographer => photographer.id == identifiant)
-    console.log(photographer);
     if (!photographer) {
         window.location.href = '/index.html'
     }
+    let photographerDom = () => {
+        const picture = `assets/photographers/${photographer.portrait}`;
+        let photographerHeader = document.querySelector(".photograph-header")
+        let photographerInfo = document.querySelector(".photograph-information")
+        let h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
+        const h3 = document.createElement('h3');
+        const img = document.createElement('img');
+        photographerInfo.appendChild(h1);
+        h1.textContent = photographer.name;
+        localStorage.setItem('name', photographer.name);
+        photographerInfo.appendChild(h2);
+        h2.textContent = photographer.city.concat(", ", photographer.country)
+        photographerInfo.appendChild(h3);
+        h3.textContent = photographer.tagline;
+        photographerHeader.appendChild(img);
+        img.setAttribute("src", picture)
+        img.setAttribute("alt", "description image")
+
+    }
+
+    photographerDom()
+    return photographer
+}
+let getMedia = (photographer) => {
     let mediaPhotographers = photographers.media;
     mediaPhotographers.forEach(mediaPhotographer => {
-        const image = `assets/images/${mediaPhotographer.image}`
-        const videos = `assets/images/${mediaPhotographer.video}`;
-
+        const image = `assets/images/${photographer.name}/${mediaPhotographer.image}`
+        const videos = `assets/images/${photographer.name}/${mediaPhotographer.video}`;
         if (mediaPhotographer.photographerId == identifiant) {
             let galeries = document.getElementById("galerie")
             let galerieCase = document.createElement("div")
@@ -24,62 +47,34 @@ let getPhotographer = () => {
             const heart = document.createElement("div")
             let link = document.createElement("a")
             heart.setAttribute("class", "heart")
-
             galerieCase.setAttribute("class", "galerie-case")
             link.setAttribute("href", "#")
             like.setAttribute("class", "like")
             galeries.appendChild(galerieCase);
             galerieCase.appendChild(link);
-
             if (mediaPhotographer.image) {
                 img.setAttribute("src", image)
                 img.setAttribute("alt", mediaPhotographer.title)
                 link.appendChild(img);
-
             } else {
                 source.setAttribute("src", videos)
                 source.setAttribute("type", "video/mp4")
                 video.setAttribute("controls", "")
-
                 video.appendChild(source);
                 link.appendChild(video);
-
             }
-
             const p = document.createElement('p');
-            const numberLike = document.createElement('p');
             galerieCase.appendChild(like);
             like.appendChild(p);
             // like.appendChild(numberLike);
             like.appendChild(heart);
-
             p.textContent = mediaPhotographer.title
             // numberLike.textContent = mediaPhotographer.likes
-
             heart.innerHTML = `<p>${mediaPhotographer.likes}</p> <div class="heart1"><i class="fas fa-heart fa-2x"></i></div>
            `
         }
-
     });
-    const picture = `assets/photographers/${photographer.portrait}`;
-    let photographerHeader = document.querySelector(".photograph-header")
-    let photographerInfo = document.querySelector(".photograph-information")
-    let h1 = document.createElement('h1');
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
-    const img = document.createElement('img');
-    let photographerDom = () => {
-
-        photographerInfo.appendChild(h1);
-        h1.textContent = photographer.name;
-        photographerInfo.appendChild(h2);
-        h2.textContent = photographer.city.concat(", ", photographer.country)
-        photographerInfo.appendChild(h3);
-        h3.textContent = photographer.tagline;
-        photographerHeader.appendChild(img);
-        img.setAttribute("src", picture)
-        img.setAttribute("alt", "description image")
-    }
-    photographerDom()
 }
-getPhotographer()
+let photographer = getPhotographer()
+
+getMedia(photographer)
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,9 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const namePhotographer = localStorage.getItem('name');
+    const modal = document.getElementById("contact_modal")
+    let name = document.querySelector(".name")
     modal.style.display = "block";
+    name.textContent = namePhotographer
 }
 
 function closeModal() {
